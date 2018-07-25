bd90a6522ff58fde270382a78363a82651e5ecc7
Lucas Lima <lucas.lima@otimizeit.com.br>
2018-07-25 11:30:52 -0300

    login simple

---
+++ app/src/main/java/luanRocha/com/br/templates_login/Login_simple.java
@@ -0,0 +1,13 @@
+package luanRocha.com.br.templates_login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_simple extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_simple);
+    }
+}
